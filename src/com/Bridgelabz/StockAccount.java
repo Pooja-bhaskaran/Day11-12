ff18655fda0410a8e1eee4fd63d9537df1795b03
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-06-07 18:56:15 +0530

    ADDED : OOPS assignment

---
+++ src/com/Bridgelabz/StockAccount.java
@@ -0,0 +1,29 @@
+package com.Bridgelabz;
+import java.util.Scanner;
+public class StockAccount {
+    static Scanner scanner = new Scanner(System.in);
+    private double balance;
+
+    public StockAccount(double balance) {
+        super();
+        this.balance = balance;
+    }
+
+    public void debit(double balance) {
+        System.out.println("enter amount to withdraw");
+        double withdraw = scanner.nextDouble();
+        if (withdraw > balance)
+            System.out.println("withdraw exceeds the balance please enter correct amount");
+        else
+            balance = balance - withdraw;
+        System.out.println("Remaining balance is " + balance);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the balance to mention in the account");
+        StockAccount account = new StockAccount(scanner.nextDouble());
+        account.debit(account.balance);
+    }
+
+}
